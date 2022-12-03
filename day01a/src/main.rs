ed544ab9de8c56ac0bd8c38162da0654c71cc348
Daryl Pinto <daryl.j.pinto@gmail.com>
2022-12-03 17:19:48 -0500

    day 1 part a

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt");
+    let answer = puzzle(input);
+    println!("{answer}");
+}
+
+fn puzzle(input: &str) -> i32 {
+    let calorie_list = input
+        .lines()
+        .map(|line| line.parse::<i32>().ok())
+        .collect::<Vec<_>>();
+
+    let calorie_sum_list = calorie_list
+        .split(|item| item.is_none())
+        .map(|x| x.iter().filter_map(|&y| y).sum::<i32>())
+        .collect::<Vec<_>>();
+
+    calorie_sum_list.into_iter().max().unwrap_or_default()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day01a() {
+        let input = include_str!("../sample.txt");
+        let answer = puzzle(input);
+        assert_eq!(answer, 24000);
+    }
 }
